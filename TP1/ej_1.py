91d801bc82e45d1c60e0e985d9e53e02e714c115
Santiago <srivas@itba.edu.ar>
2024-09-04 19:42:59 -0300

    Changed order of file

--- TP1/ej_1.py
+++ TP1/ej_1.py
@@ -1,5 +1,34 @@
 import pandas as pd
 
+
+def calcular_probabilidades_condicionales(df, total_personas):
+    probabilidades = {}
+    for columna in df.columns:
+        if columna != "Nacionalidad":
+            prob_1 = df[columna].sum() / total_personas
+            probabilidades[columna] = prob_1
+    return probabilidades
+
+
+def clasificar(vector, prob_cond_ingleses, prob_cond_escoceses, prior_ingleses, prior_escoces):
+    prob_ingleses = prior_ingleses
+    prob_escoces = prior_escoces
+
+    for columna, valor in vector.items():
+        if valor == 1:
+            prob_ingleses *= prob_cond_ingleses[columna]
+            prob_escoces *= prob_cond_escoceses[columna]
+        else:
+            prob_ingleses *= (1 - prob_cond_ingleses[columna])
+            prob_escoces *= (1 - prob_cond_escoceses[columna])
+
+    # Determinar la clase con mayor probabilidad
+    if prob_ingleses > prob_escoces:
+        return "Inglés"
+    else:
+        return "Escocés"
+
+
 df = pd.read_excel('data/PreferenciasBritanicos.xlsx')
 
 print(df)
@@ -21,15 +50,6 @@ prior_ingleses = total_ingleses / total_personas
 prior_escoces = total_escoceses / total_personas
 
 
-def calcular_probabilidades_condicionales(df, total_personas):
-    probabilidades = {}
-    for columna in df.columns:
-        if columna != "Nacionalidad":
-            prob_1 = df[columna].sum() / total_personas
-            probabilidades[columna] = prob_1
-    return probabilidades
-
-
 prob_cond_ingleses = calcular_probabilidades_condicionales(
     ingleses, total_ingleses)
 prob_cond_escoceses = calcular_probabilidades_condicionales(
@@ -37,25 +57,6 @@ prob_cond_escoceses = calcular_probabilidades_condicionales(
 
 print("P_cond ingleses", prob_cond_ingleses)
 print("P_cond escoceses", prob_cond_escoceses)
-
-
-def clasificar(vector, prob_cond_ingleses, prob_cond_escoceses, prior_ingleses, prior_escoces):
-    prob_ingleses = prior_ingleses
-    prob_escoces = prior_escoces
-
-    for columna, valor in vector.items():
-        if valor == 1:
-            prob_ingleses *= prob_cond_ingleses[columna]
-            prob_escoces *= prob_cond_escoceses[columna]
-        else:
-            prob_ingleses *= (1 - prob_cond_ingleses[columna])
-            prob_escoces *= (1 - prob_cond_escoceses[columna])
-
-    # Determinar la clase con mayor probabilidad
-    if prob_ingleses > prob_escoces:
-        return "Inglés"
-    else:
-        return "Escocés"
 
 
 x_1 = {
