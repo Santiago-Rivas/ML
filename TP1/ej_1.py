2ea3f3610cad36463a48cd25d791cfa497fe8909
Santiago <srivas@itba.edu.ar>
2024-09-10 15:25:47 -0300

    fixed error in laplace correction

--- TP1/ej_1.py
+++ TP1/ej_1.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+NUMBER_OF_CLASSES = 2
+
 
 def calcular_probabilidades_condicionales(df, total_personas):
     probabilidades = {}
-    k = len(df.columns) - 1
+    k = NUMBER_OF_CLASSES
     for columna in df.columns:
         if columna != "Nacionalidad":
             prob_1 = (df[columna].sum() + 1) / (total_personas + k)
@@ -32,12 +34,12 @@ def clasificar(vector, prob_cond_ingleses, prob_cond_escoceses, prior_ingleses, 
 
 def main():
     df = pd.read_excel('data/PreferenciasBritanicos.xlsx')
-    #print(df)
+    # print(df)
 
     ingleses = df[df["Nacionalidad"] == "I"]
     escoceses = df[df["Nacionalidad"] == "E"]
-    #print(ingleses)
-    #print(escoceses)
+    # print(ingleses)
+    # print(escoceses)
 
     total_ingleses = len(ingleses)
     total_escoceses = len(escoceses)
@@ -49,8 +51,10 @@ def main():
     prior_ingleses = total_ingleses / total_personas
     prior_escoces = total_escoceses / total_personas
 
-    prob_cond_ingleses = calcular_probabilidades_condicionales(ingleses, total_ingleses)
-    prob_cond_escoceses = calcular_probabilidades_condicionales(escoceses, total_escoceses)
+    prob_cond_ingleses = calcular_probabilidades_condicionales(
+        ingleses, total_ingleses)
+    prob_cond_escoceses = calcular_probabilidades_condicionales(
+        escoceses, total_escoceses)
     print("P_cond ingleses", prob_cond_ingleses)
     print("P_cond escoceses", prob_cond_escoceses)
 
@@ -61,7 +65,8 @@ def main():
         "avena": 1,
         "futbol": 0
     }
-    resultado = clasificar(x_1, prob_cond_ingleses,prob_cond_escoceses, prior_ingleses, prior_escoces)
+    resultado = clasificar(x_1, prob_cond_ingleses,
+                           prob_cond_escoceses, prior_ingleses, prior_escoces)
     print(f"El vector {x_1} es clasificado como: {resultado}")
 
     x_2 = {
@@ -71,10 +76,10 @@ def main():
         "avena": 0,
         "futbol": 1
     }
-    resultado = clasificar(x_2, prob_cond_ingleses,prob_cond_escoceses, prior_ingleses, prior_escoces)
+    resultado = clasificar(x_2, prob_cond_ingleses,
+                           prob_cond_escoceses, prior_ingleses, prior_escoces)
     print(f"El vector {x_2} es clasificado como: {resultado}")
 
 
 if __name__ == '__main__':
     main()
-
